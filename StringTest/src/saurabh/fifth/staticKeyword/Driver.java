28174818eb908555c6453e102b870fa0fb196c84
Saurabh <Saurabh@OmSai>
2018-06-21 13:04:54 +0530

    Added the reason for program that static field has only one instance and it will be shared by all.

--- StringTest/src/saurabh/fifth/staticKeyword/Driver.java
+++ StringTest/src/saurabh/fifth/staticKeyword/Driver.java
@@ -1,5 +1,5 @@
 package saurabh.fifth.staticKeyword;
-
+//Reference: https://stackoverflow.com/questions/413898/what-does-the-static-keyword-do-in-a-class
 class StaticExample {
 	int count = 0;
 	StaticExample() {
@@ -24,14 +24,17 @@ class StaticExample3{
 }
 public class Driver{
 	public static void main(String[] args) {
-		StaticExample se1 = new StaticExample();
-		StaticExample se2 = new StaticExample();
-		StaticExample se3 = new StaticExample();
-			StaticExample2 se21 = new StaticExample2();
-			StaticExample2 se22 = new StaticExample2();
-			StaticExample2 se23 = new StaticExample2();
-			StaticExample3 se31 = new StaticExample3();
-			StaticExample3 se32 = new StaticExample3();
-			StaticExample3 se33 = new StaticExample3();
+		StaticExample se1 = new StaticExample();//1
+		StaticExample se2 = new StaticExample();//1
+		StaticExample se3 = new StaticExample();//1
+			StaticExample2 se21 = new StaticExample2();//1
+			StaticExample2 se22 = new StaticExample2();//2
+			StaticExample2 se23 = new StaticExample2();//3
+			/*It means that only one instance of a static field exists[1] even if you create
+			 * a million instances of the class or you don't create any. It will be shared 
+			 * by all instances.*/
+			StaticExample3 se31 = new StaticExample3();//0
+			StaticExample3 se32 = new StaticExample3();//0
+			StaticExample3 se33 = new StaticExample3();//0
 		}
 }
